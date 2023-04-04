d7e1a1cee9f16468d36599907e653a2b23f0fa81
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-04-04 16:34:09 +0800

    feat(prelude): add global imports for type-fest

--- packages/prelude/_src/_global.ts
+++ packages/prelude/_src/_global.ts
@@ -5,11 +5,11 @@ import './_global.ext.js'
 /**
  * @tsplus global
  */
-// rome-ignore lint/correctness/noUnusedVariables: ignore
 import { none, some } from '@effect/data/Option'
+
+import './_global/type-fest.js'
 
 /**
  * @tsplus global
  */
-// rome-ignore lint/correctness/noUnusedVariables: ignore
 import { R } from '@daotl-effect/prelude/_global/remeda'
---
+++ packages/prelude/_src/_global/type-fest.ts
@@ -0,0 +1,213 @@
+// https://github.com/sindresorhus/type-fest/blob/5374588a88ee643893784f66367bc26b8e6509ec/index.d.ts
+
+/**
+ * @tsplus global
+ */
+import type {
+  // Basic
+  // primitive: https://github.com/sindresorhus/type-fest/blob/5374588a88ee643893784f66367bc26b8e6509ec/source/primitive.d.ts
+  Primitive,
+  // typed-array: https://github.com/sindresorhus/type-fest/blob/5374588a88ee643893784f66367bc26b8e6509ec/source/typed-array.d.ts
+  TypedArray,
+  // basic: https://github.com/sindresorhus/type-fest/blob/5374588a88ee643893784f66367bc26b8e6509ec/source/basic.d.ts
+  Class,
+  Constructor,
+  AbstractClass,
+  AbstractConstructor,
+  JsonObject,
+  JsonArray,
+  JsonPrimitive,
+  JsonValue,
+  // observable-like: https://github.com/sindresorhus/type-fest/blob/5374588a88ee643893784f66367bc26b8e6509ec/source/observable-like.d.ts
+  Unsubscribable,
+  Observer,
+  ObservableLike,
+  // Utilities
+  // empty-object
+  EmptyObject,
+  IsEmptyObject,
+  // except
+  Except,
+  // tagged-union
+  TaggedUnion,
+  // writable
+  Writable,
+  // writable-deep
+  WritableDeep,
+  // merge
+  Merge,
+  // merge-deep
+  MergeDeep,
+  MergeDeepOptions,
+  // merge-exclusive
+  MergeExclusive,
+  // require-at-least-one
+  RequireAtLeastOne,
+  // require-exactly-one
+  RequireExactlyOne,
+  // require-all-or-none
+  RequireAllOrNone,
+  // omit-index-signature
+  OmitIndexSignature,
+  // pick-index-signature
+  PickIndexSignature,
+  // partial-deep
+  PartialDeep,
+  PartialDeepOptions,
+  // partial-on-undefined-deep
+  PartialOnUndefinedDeep,
+  PartialOnUndefinedDeepOptions,
+  // readonly-deep
+  ReadonlyDeep,
+  // literal-union
+  LiteralUnion,
+  // promisable
+  Promisable,
+  // opaque
+  Opaque,
+  UnwrapOpaque,
+  // invariant-of
+  InvariantOf,
+  // set-optional
+  SetOptional,
+  // set-required
+  SetRequired,
+  // set-non-nullable
+  SetNonNullable,
+  // value-of
+  ValueOf,
+  // async-return-type
+  AsyncReturnType,
+  // conditional-except
+  ConditionalExcept,
+  // conditional-keys
+  ConditionalKeys,
+  // conditional-pick
+  ConditionalPick,
+  // conditional-pick-deep
+  ConditionalPickDeep,
+  ConditionalPickDeepOptions,
+  // union-to-intersection
+  UnionToIntersection,
+  // stringified
+  Stringified,
+  // fixed-length-array
+  FixedLengthArray,
+  // multidimensional-array
+  MultidimensionalArray,
+  // multidimensional-readonly-array
+  MultidimensionalReadonlyArray,
+  // iterable-element
+  IterableElement,
+  // entry
+  Entry,
+  // entries
+  Entries,
+  // set-return-type
+  SetReturnType,
+  // asyncify
+  Asyncify,
+  // simplify
+  Simplify,
+  // jsonify
+  Jsonify,
+  // jsonifiable
+  Jsonifiable,
+  // schema
+  Schema,
+  // literal-to-primitive
+  LiteralToPrimitive,
+  // literal-to-primitive-deep
+  // LiteralToPrimitiveDeep,
+  // numeric
+  PositiveInfinity,
+  NegativeInfinity,
+  Finite,
+  Integer,
+  Float,
+  NegativeFloat,
+  Negative,
+  NonNegative,
+  NegativeInteger,
+  NonNegativeInteger,
+  // string-key-of
+  StringKeyOf,
+  // exact
+  Exact,
+  // readonly-tuple
+  ReadonlyTuple,
+  // optional-keys-of
+  OptionalKeysOf,
+  // has-optional-keys
+  HasOptionalKeys,
+  // required-keys-of
+  RequiredKeysOf,
+  // has-required-keys
+  HasRequiredKeys,
+  // spread
+  Spread,
+  // tuple-to-union
+  TupleToUnion,
+  // is-equal
+  IsEqual,
+  // is-literal
+  IsLiteral,
+  IsStringLiteral,
+  IsNumericLiteral,
+  IsBooleanLiteral,
+  IsSymbolLiteral,
+  // Template literal types
+  // camel-case
+  CamelCase,
+  // camel-cased-properties
+  CamelCasedProperties,
+  // camel-cased-properties-deep
+  CamelCasedPropertiesDeep,
+  // kebab-case
+  KebabCase,
+  // kebab-cased-properties
+  KebabCasedProperties,
+  // kebab-cased-properties-deep
+  KebabCasedPropertiesDeep,
+  // pascal-case
+  PascalCase,
+  // pascal-cased-properties
+  PascalCasedProperties,
+  // pascal-cased-properties-deep
+  PascalCasedPropertiesDeep,
+  // snake-case
+  SnakeCase,
+  // snake-cased-properties
+  SnakeCasedProperties,
+  // snake-cased-properties-deep
+  SnakeCasedPropertiesDeep,
+  // screaming-snake-case
+  ScreamingSnakeCase,
+  // delimiter-case
+  DelimiterCase,
+  // delimiter-cased-properties
+  DelimiterCasedProperties,
+  // delimiter-cased-properties-deep
+  DelimiterCasedPropertiesDeep,
+  // join
+  Join,
+  // split
+  Split,
+  // trim
+  Trim,
+  // replace
+  Replace,
+  // includes
+  Includes,
+  // get
+  Get,
+  // last-array-element
+  LastArrayElement,
+  // Miscellaneous
+  // global-this
+  GlobalThis,
+  // package-json
+  PackageJson,
+  // tsconfig-json
+  TsConfigJson,
+} from 'type-fest'
