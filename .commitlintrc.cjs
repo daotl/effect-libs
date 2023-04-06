f074ff94fb44df9b87ffef295e39d5b2efd9a933
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-04-06 12:07:10 +0800

    setup(root): add Lefthook and commitlint configs

---
+++ .commitlintrc.cjs
@@ -0,0 +1,41 @@
+const { getPackages } = require('commitlint-config-pnpm-workspace/scope-enhanced')
+
+const pkgs = (ctx) =>
+  getPackages(ctx).then((pkgs) =>
+    pkgs.concat(['root', 'docker-compose', 'k8s']),
+  )
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['scope-enhanced'],
+  ignores: [
+    (message) => message.startsWith('Merge ') || message.startsWith('WIP: '),
+  ],
+  rules: {
+    'type-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'WIP',
+        'setup',
+        'build',
+        'docker',
+        'k8s',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'scope-empty': [2, 'never'],
+    'scope-enum': async (ctx) => [0, 'always', await pkgs(ctx)],
+    'scope-enum-enhanced': async (ctx) => [2, 'always', await pkgs(ctx)],
+  },
+}
