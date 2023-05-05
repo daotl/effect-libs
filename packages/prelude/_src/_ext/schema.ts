d554b94efff6cdcf0b5e031727077623f3f13f98
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-05-05 18:21:27 +0800

    feat(prelude): add custome types and utils to Schema

---
+++ packages/prelude/_src/_ext/schema.ts
@@ -0,0 +1,136 @@
+import * as S from '@effect/schema/Schema'
+
+/**
+ * @tsplus type effect/schema/Schema
+ */
+// rome-ignore lint/suspicious/noExplicitAny: ignore
+export type Any = Schema<any>
+
+export type StructArgs = Parameters<typeof S.struct>[0]
+
+/**
+ * @tsplus type effect/schema/Schema
+ */
+export type Struct<Fields extends StructArgs> = ReturnType<
+  typeof S.struct<Fields>
+>
+
+/**
+ * @tsplus type effect/schema/Schema
+ */
+export type Nullable<From, To = From> = ReturnType<typeof S.nullable<From, To>>
+
+/**
+ * @tsplus type effect/schema/Schema
+ */
+export type Optional<From, To = From> = ReturnType<typeof S.optional<From, To>>
+
+// Nullish<From, To> = Optional<Nullable<From, To>>
+/**
+ * @tsplus type effect/schema/Schema
+ */
+export type Nullish<From, To = From> = Optional<Nullable<From, To>>
+
+// Make properties of the specified keys or all keys nullable
+/**
+ * @tsplus type effect/schema/Schema
+ */
+export type NullableProperties<
+  // rome-ignore lint/suspicious/noExplicitAny: ignore
+  I extends { [K in keyof A]: any },
+  A extends StructArgs,
+  Key extends keyof A = keyof A,
+> = Struct<{
+  [K in keyof A]: K extends Key ? Nullable<I[K], A[K]> : A[K]
+}>
+
+// Make properties of the specified keys or all keys optional
+/**
+ * @tsplus type effect/schema/Schema
+ */
+export type OptionalProperties<
+  // rome-ignore lint/suspicious/noExplicitAny: ignore
+  I extends { [K in keyof A]: any },
+  A extends StructArgs,
+  Key extends keyof A = keyof A,
+> = Struct<{
+  [K in keyof A]: K extends Key ? Optional<I[K], A[K]> : A[K]
+}>
+
+// Make properties of the specified keys or all keys nullish
+/**
+ * @tsplus type effect/schema/Schema
+ */
+export type NullishProperties<
+  // rome-ignore lint/suspicious/noExplicitAny: ignore
+  I extends { [K in keyof A]: any },
+  A extends StructArgs,
+  Key extends keyof A = keyof A,
+> = Struct<{
+  [K in keyof A]: K extends Key ? Nullish<I[K], A[K]> : A[K]
+}>
+
+/**
+ * @tsplus static effect/schema/Schema nullish
+ */
+export const nullish = flow(S.nullable, S.optional)
+
+// Make properties of the specified keys or all keys nullable
+// Ref: https://github.com/Effect-TS/schema/blob/9cdb0ea2227ac6efcad68587268c9ef80423c309/src/S.ts#L705
+/**
+ * @tsplus static effect/schema/Schema nullableProperties
+ */
+export const nullableProperties = <
+  // rome-ignore lint/suspicious/noExplicitAny: ignore
+  I extends { [K in keyof A]: any },
+  A extends StructArgs,
+  Key extends keyof A = keyof A,
+>(
+  schema: S.Schema<I, A>,
+  keys?: Key[],
+) =>
+  S.struct(
+    R.mapValues(S.getPropertySignatures(schema), (v, k) =>
+      !keys || (keys as (keyof A)[]).includes(k) ? S.nullable(v) : v,
+    ),
+  ) as NullableProperties<I, A, Key>
+
+// Make properties of the specified keys or all keys optional
+// Ref: https://github.com/Effect-TS/schema/blob/9cdb0ea2227ac6efcad68587268c9ef80423c309/src/S.ts#L705
+/**
+ * @tsplus static effect/schema/Schema optionalProperties
+ */
+export const optionalProperties = <
+  // rome-ignore lint/suspicious/noExplicitAny: ignore
+  I extends { [K in keyof A]: any },
+  A extends StructArgs,
+  Key extends keyof A = keyof A,
+>(
+  schema: S.Schema<I, A>,
+  keys?: Key[],
+) =>
+  S.struct(
+    R.mapValues(S.getPropertySignatures(schema), (v, k) =>
+      !keys || (keys as (keyof A)[]).includes(k) ? S.optional(v) : v,
+    ),
+  ) as OptionalProperties<I, A, Key>
+
+// Make properties of the specified keys or all keys nullish
+// Ref: https://github.com/Effect-TS/schema/blob/9cdb0ea2227ac6efcad68587268c9ef80423c309/src/S.ts#L705
+/**
+ * @tsplus static effect/schema/Schema nullishProperties
+ */
+export const nullishProperties = <
+  // rome-ignore lint/suspicious/noExplicitAny: ignore
+  I extends { [K in keyof A]: any },
+  A extends StructArgs,
+  Key extends keyof A = keyof A,
+>(
+  schema: S.Schema<I, A>,
+  keys?: Key[],
+) =>
+  S.struct(
+    R.mapValues(S.getPropertySignatures(schema), (v, k) =>
+      !keys || (keys as (keyof A)[]).includes(k) ? nullish(v) : v,
+    ),
+  ) as NullishProperties<I, A, Key>
--- packages/prelude/_src/_global/schema.ts
+++ packages/prelude/_src/_global/schema.ts
@@ -1,7 +1,7 @@
-import * as NsSchema from '@effect/schema/Schema'
-import type { Schema as TypeSchema } from '@effect/schema/Schema'
+import * as S from '@effect/schema/Schema'
+import * as Ext from '../_ext/schema.js'
 
-export type Schema<From, To = From> = TypeSchema<From, To>
+export type Schema<From, To = From> = S.Schema<From, To>
 
 export namespace Schema {
   export type From<
@@ -9,14 +9,64 @@ export namespace Schema {
       // rome-ignore lint/suspicious/noExplicitAny: ignore
       readonly From: (..._: any) => any
     },
-  > = NsSchema.From<S>
+  > = S.From<S>
 
   export type To<
     S extends {
       // rome-ignore lint/suspicious/noExplicitAny: ignore
       readonly To: (..._: any) => any
     },
-  > = NsSchema.To<S>
+  > = S.To<S>
+
+  /* Custom types */
+
+  export type Any = Ext.Any
+
+  export type Struct<
+    Fields extends Record<
+      PropertyKey,
+      // rome-ignore lint/suspicious/noExplicitAny: ignore
+      | S.Schema<any, any>
+      | S.Schema<never, never>
+      // rome-ignore lint/suspicious/noExplicitAny: ignore
+      | S.PropertySignature<any, boolean, any, boolean>
+      | S.PropertySignature<never, boolean, never, boolean>
+    >,
+  > = Ext.Struct<Fields>
+
+  export type Nullable<From, To = From> = Ext.Nullable<From, To>
+
+  export type Optional<From, To = From> = Ext.Optional<From, To>
+
+  export type Nullish<From, To = From> = Ext.Nullish<From, To>
+
+  export type NullableProperties<
+    // rome-ignore lint/suspicious/noExplicitAny: ignore
+    I extends { [K in keyof A]: any },
+    A extends Ext.StructArgs,
+    Key extends keyof A = keyof A,
+  > = Ext.NullableProperties<I, A, Key>
+
+  export type OptionalProperties<
+    // rome-ignore lint/suspicious/noExplicitAny: ignore
+    I extends { [K in keyof A]: any },
+    A extends Ext.StructArgs,
+    Key extends keyof A = keyof A,
+  > = Ext.OptionalProperties<I, A, Key>
+
+  export type NullishProperties<
+    // rome-ignore lint/suspicious/noExplicitAny: ignore
+    I extends { [K in keyof A]: any },
+    A extends Ext.StructArgs,
+    Key extends keyof A = keyof A,
+  > = Ext.NullishProperties<I, A, Key>
 }
 
-export const Schema = NsSchema
+export const Schema = {
+  ...S,
+  /* Custom utils */
+  nullish: Ext.nullish,
+  nullableProperties: Ext.nullableProperties,
+  optionalProperties: Ext.optionalProperties,
+  nullishProperties: Ext.nullishProperties,
+}
