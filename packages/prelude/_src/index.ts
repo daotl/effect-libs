e91f843ef555e513506e83ea21a3273d45f63a7d
Nex <hitnexup@gmail.com>
2023-10-17 17:20:45 +0800

    chore: fix dep biome -> @biomejs/biome

--- packages/prelude/_src/index.ts
+++ packages/prelude/_src/index.ts
@@ -1,2 +1,14 @@
 export * from './runtime.js'
 export { basicRuntime } from './basicRuntime.js'
+
+const arr = [2, 3, 4, 2, 3, 5]
+console.log(arr.randomElement())
+console.log(arr.randomElement())
+console.log(arr.randomElement())
+
+import { NonEmptyArray } from 'effect/ReadonlyArray'
+
+const nera = ReadonlyArray.make(1, 2, 3)
+
+const goodType = arr.toNonEmptyArray
+const badType = [].toNonEmptyArray
